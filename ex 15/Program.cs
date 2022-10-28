abc8f7e83a67e5cbbfc083af4738d77dd99d897e
alpryakh <alpryakh@gmail.com>
2022-10-28 23:45:20 +0300

    ex 15

---
+++ ex 15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter number:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num==6 | num==7)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
