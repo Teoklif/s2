161dae590d47c746d1027807516be8cc163846f1
alpryakh <alpryakh@gmail.com>
2022-10-28 23:39:52 +0300

    ex 13

--- ex 13/Program.cs
+++ ex 13/Program.cs
@@ -1,1 +1,12 @@
-﻿int num = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Enter number:");
+int num = Convert.ToInt32(Console.ReadLine());
+string num_text = Convert.ToString(num);
+if (num_text.Length < 3)
+{
+  Console.WriteLine("Nothing to show");
+}
+else 
+{
+  
+  Console.WriteLine(num_text[2]);
+}
